a99d1952a2ece809d00268a6bc9a39d982b52710
Jacek Tomaszewski <jacek@jtom.me>
2021-02-10 23:38:11 +1100

    fix: Use normal way for getting the file dir, that works everywhere

--- registration.php
+++ registration.php
@@ -6,5 +6,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Groomershop_FullBreadcrumbs',
-    isset($file) && realpath($file) == __FILE__ ? dirname($file) : __DIR__
+    __DIR__
 );
